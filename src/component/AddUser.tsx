2fa2a700bfe59d9f2cede85d0d57e1c9440fa9bd
Phinehas Enakireru <phinehasenakireru@gmail.com>
2023-08-06 15:55:37 +0100

    update: [AddUser] implement read values on form submit

--- src/component/AddUser.tsx
+++ src/component/AddUser.tsx
@@ -1,12 +1,41 @@
-import {  } from "react";
+import { useRef } from "react";
+import axios from "axios";
 
 export const AddUser = () => {
-  const roleList = ["User", "Group_Leader", "Supervisor"];
+  const firstNameRef = useRef<HTMLInputElement>(null);
+  const lastNameRef = useRef<HTMLInputElement>(null);
+  const roleUserRef = useRef<HTMLInputElement>(null);
+  const roleGroupLeaderRef = useRef<HTMLInputElement>(null);
+  const roleSupervisorRef = useRef<HTMLInputElement>(null);
+  const roleMDRef = useRef<HTMLInputElement>(null);
+  const rolesRef = [roleUserRef, roleGroupLeaderRef, roleMDRef, roleSupervisorRef];
+  const groupNameRef = useRef<HTMLSelectElement>(null);
+
+  const postUsers = async () => {
+    const rolesRefValues = rolesRef
+      .filter((roleRef) => roleRef.current?.checked)
+      .map((roleRef) => roleRef.current?.value);
+    const user = {
+      firstName: firstNameRef.current?.value,
+      lastName: lastNameRef.current?.value,
+      password: [
+        firstNameRef.current?.value.charAt(0),
+        lastNameRef.current?.value.charAt(0),
+        "pword",
+      ].join(""),
+      roleNames: rolesRefValues,
+      groupName: groupNameRef.current?.value,
+    };
+
+    console.log(user);
+    // const { data } = await axios.post(``, [user]);
+  };
+
   const groupList = [
     "YEAR_1",
     "YEAR_2",
     "YEAR_3",
-    "YEAR_3",
+    "YEAR_4",
     "YEAR_5",
     "YEAR_6",
     "YEAR_7",
@@ -22,32 +51,49 @@ export const AddUser = () => {
       <form action="">
         <div className="form-control">
           <label htmlFor="">First Name</label>
-          <input type="text" className="block" required />
+          <input type="text" ref={firstNameRef} className="block" required />
         </div>
         <div className="form-control">
           <label htmlFor="">Last Name</label>
-          <input type="text" className="block" required />
+          <input type="text" ref={lastNameRef} className="block" required />
         </div>
         <div className="form-control">
-          {roleList.map((role) => {
-            return (
-              <>
-                <label htmlFor="">{role}</label>
-                <input type="checkbox" value={role}></input>
-              </>
-            );
-          })}
+          <label htmlFor="">USER</label>
+          <input type="checkbox" ref={roleUserRef} value={`USER`}></input>
+          <label htmlFor="">GROUP LEAD</label>
+          <input
+            type="checkbox"
+            ref={roleGroupLeaderRef}
+            value={`GROUP_LEADER`}
+          ></input>
+          <label htmlFor="">MD</label>
+          <input type="checkbox" ref={roleMDRef} value={`MD`}></input>
+          <label htmlFor="">SUPERVISOR</label>
+          <input
+            type="checkbox"
+            ref={roleSupervisorRef}
+            value={`SUPERVISOR`}
+          ></input>
         </div>
         <div className="form-control">
           <label htmlFor="">Group</label>
-          <select name="" id="">
+          <select ref={groupNameRef} id="">
             {groupList.map((group) => (
-              <option value={group}>{group}</option>
+              <option key={group} value={group}>
+                {group}
+              </option>
             ))}
           </select>
         </div>
         <div className="form-control">
-          <input type="submit" className="block" />
+          <input
+            type="submit"
+            className="block"
+            onClick={(e) => {
+              e.preventDefault();
+              postUsers();
+            }}
+          />
         </div>
       </form>
     </div>
