da0bed6be483a913adbf26d554c040415372d7c8
Phinehas Enakireru <phinehasenakireru@gmail.com>
2023-08-11 23:06:51 +0100

    update: [DataTable] update how data items are diplayed This commit also makes update to styling of the table.

--- src/component/DataTable.tsx
+++ src/component/DataTable.tsx
@@ -1,22 +1,36 @@
-import { TableData } from "../Interface";
+import { TableData } from "../interfaces";
+import { convertObjectToString } from "../utils";
 
 export const DataTable = (data: TableData) => {
   return (
     <div>
       <table>
         <thead>
-          <tr>
-            {data.headings.map((heading) => (
-              <th key={data.headings.indexOf(heading)}>{heading}</th>
+          <tr className="border">
+            {data.headings.map((heading, index) => (
+              <th
+                key={index}
+                className="p-1 sm:p-3 border-solid border-8 border-white bg-slate-300"
+              >
+                {heading}
+              </th>
             ))}
           </tr>
         </thead>
         <tbody>
-          {data.items.map((item) => (
-            <tr key={data.items.indexOf(item)}>
-              {item.map((i) => (
-                <td key={item.indexOf(i)}>{i}</td>
-              ))}
+          {data.items.map((item, index) => (
+            <tr key={Object.values(item)?.[0] ?? index}>
+              {Object.values(item).map((value, index) => {
+                value =
+                  typeof value === "object"
+                    ? convertObjectToString(value)
+                    : value;
+                return (
+                  <td key={index} className="py-3 px-9 border-4">
+                    {value}
+                  </td>
+                );
+              })}
             </tr>
           ))}
         </tbody>
