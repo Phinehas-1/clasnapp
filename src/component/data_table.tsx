07b311f79b294a45687fe502566ab4b38eefa5ee
Phinehas Enakireru <phinehasenakireru@gmail.com>
2023-08-22 22:31:52 +0100

    refactor: Redo user table population logic.

--- src/component/data_table.tsx
+++ src/component/data_table.tsx
@@ -12,14 +12,13 @@ export const DataTable = (data: TableData) => {
           <thead>
             <tr className="border">
               {data.headings.map(
-                (heading, index) =>
-                  index != 0 && (
+                (heading, index) => (
                     <th
                       key={index}
                       className={`px-4 py-2 w-max min-w-10 border-solid border-8 border-white bg-slate-300 text-center ${
-                        index === 1 &&
+                        index === 0 &&
                         `text-blue-800 absolute inline-block md:relative md:table-cell border-t-0 md:border-t-8 shadow-slate-700 shadow-md z-10`
-                      } ${index === 2 && `pl-44 md:pl-3`}`}
+                      } ${index === 1 && `pl-44 md:pl-3`}`}
                     >
                       {heading}
                     </th>
@@ -29,24 +28,22 @@ export const DataTable = (data: TableData) => {
           </thead>
           <tbody>
             {data.items.map((item, index) => (
-              <tr key={Object.values(item)?.[0] ?? index}>
+              <tr key={index}>
                 {Object.values(item).map((value, index) => {
                   value =
                     typeof value === "object"
                       ? convertObjectToString(value)
                       : value;
                   return (
-                    index != 0 && (
                       <td
                         key={index}
                         className={`py-3 w-max min-w-10 px-4 border-4 ${
-                          index === 1 &&
+                          index === 0 &&
                           `absolute inline-block md:relative md:table-cell w-32 z-10 bg-blue-100 border-white shadow-slate-700 shadow-md`
-                        } ${index === 2 && `pl-44 md:pl-3`}`}
+                        } ${index === 1 && `pl-44 md:pl-3`}`}
                       >
                         {value}
                       </td>
-                    )
                   );
                 })}
               </tr>
