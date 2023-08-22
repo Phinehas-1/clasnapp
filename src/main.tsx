95ada28027dd56ed81533bb91c004455fff3849a
Phinehas Enakireru <phinehasenakireru@gmail.com>
2023-08-22 10:38:24 +0100

    refactor: update import line for App component

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { App } from "./component/pages/app.tsx";
 import { Dashboard } from "./component/pages/dashboard.tsx";
 import { ErrorPage } from "./component/pages/error.tsx";
 import { Admin } from "./component/pages/admin.tsx";
